2480d8d9bd2eb0e49321865e5e22bb9b5e524651
Sarah26-10 <sarah.quehl@gmail.com>
2022-01-25 09:50:32 +0100

    fix: logFile

--- code/predict_vitals.py
+++ code/predict_vitals.py
@@ -21,6 +21,7 @@ def predict_vitals(args):
     model_checkpoint = args.trained_model
     batch_size = args.batch_size
     sample_data_path = args.video_path
+    print("path:  ",sample_data_path)
 
     dXsub, fs = preprocess_raw_video(sample_data_path, dim=36)
     print('dXsub shape', dXsub.shape)
@@ -40,7 +41,7 @@ def predict_vitals(args):
     pulse_pred = scipy.signal.filtfilt(b_pulse, a_pulse, np.double(pulse_pred))
     
     ##### ground truth data resampled  #######
-    truth_path = args.video_path.replace(".avi", "_vid.hdf5")
+    truth_path = args.video_path.replace(".avi", "_dataFile.hdf5")
     gound_truth_file = h5py.File(truth_path, "r")
     pulse_truth = gound_truth_file["pulse"]
     pulse_truth = detrend(np.cumsum(pulse_truth), 100)
@@ -54,16 +55,15 @@ def predict_vitals(args):
     peaks_pred, b  = np.array(signal.find_peaks(pulse_pred, prominence=0.2))
 
      ########## Plot ##################
-    # plt.figure() #subplot(211)
-    # plt.plot(pulse_pred, label='Prediction')
-    # plt.plot(peaks_truth, pulse_truth[peaks_truth], "x")
-    # plt.plot(peaks_pred, pulse_pred[peaks_pred], "o")
-    # plt.title('Pulse Prediction')
-    # #plt.subplot(212)
-    # plt.plot(pulse_truth, label='ground truth')
-    # #plt.title("Pulse truth")
-    # plt.legend()
-    # plt.show()
+    plt.figure() #subplot(211)
+    plt.plot(pulse_pred, label='Prediction')
+    plt.plot(peaks_truth, pulse_truth[peaks_truth], "x")
+    plt.plot(peaks_pred, pulse_pred[peaks_pred], "o")
+    plt.title('Pulse Prediction')
+    plt.plot(pulse_truth, label='ground truth')
+    plt.legend()
+
+    plt.figure()
     plt.subplot(211)
     plt.plot(pulse_truth, label='Ground truth')
     plt.plot(peaks_truth, pulse_truth[peaks_truth], "x")
@@ -104,6 +104,6 @@ if __name__ == "__main__":
     predict_vitals(args)
 
 
-#python code/predict_vitals.py --video_path E:\Databases\Training\COHFACE\4\1\data.avi --trained_model ./rPPG-checkpoints/testCohFace1/cv_0_epoch24_model.hdf5
+#python code/predict_vitals.py --video_path "E:\Databases\3)Testing\COHFACE\21\1\data.avi" --trained_model ./cv_0_epoch24_model.hdf5
 #./rPPG-checkpoints/testCohFace1/cv_0_epoch24_model.hdf5
 #./rPPG-checkpoints/test1/cv_0_epoch04_model.hdf5'
